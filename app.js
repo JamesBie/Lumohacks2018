c6e89a97d5ce2022db423f7b31c2f0bfa1a897af
james-bie <james-bie@d207-023-161-150.wireless.sfu.ca>
2018-09-15 16:10:36 -0700

    create skeleton site

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express"),
+	app		= express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	methodOverride	= require("method-override");
+
+var http= require ('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.get("/chat",function(req,res){
+
+	res.render("chat");
+})
+
+
+
+app.listen(process.env.PORT || 3000, process.env.IP,function(){
+
+	console.log("server started");
+})
