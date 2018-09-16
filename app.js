a2fbf00ebc348b274fda93a92ce1d02969c8094c
james-bie <james-bie@james-bies-MacBook-Pro.local>
2018-09-15 21:35:18 -0700

    add model and database track of user

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var express = require("express"),
 	mongoose = require("mongoose"),
 	methodOverride	= require("method-override");
 
+//mongoose
+var onlineUsers = require("./models/online");
+var Profile = require ('./models/profile')
 
 //chat connection 
 var http= require ('http').Server(app);
 var io = require('socket.io')(http);
+var online = 0;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -31,9 +35,27 @@ app.get("/chat",function(req,res){
 //socket related stuff
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  online = online + 1;
+  onlineUsers.create({username: online}, function(err, user){
+  	if (err){
+  		console.log(err);
+  	}else{
+  		console.log(user);
+  	}
   });
+
+  	socket.on('disconnect', function(){
+    console.log('user disconnected');
+    onlineUsers.findOneAndDelete({username:online}, function(err, user){
+    	if (err){
+    		console.log(err);
+    	}else{
+    		console.log(user);
+    		online = online -1;
+    	}
+    	});
+	});
+
 });
 
 io.on('connection', function(socket){
